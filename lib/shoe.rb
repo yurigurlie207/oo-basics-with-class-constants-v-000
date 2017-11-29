04c257097774d75e679737ff52b970c3e0638bd9
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-29 19:35:18 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.detect {|brd| brd == brand } && BRANDS != [] then
+    if BRANDS.include(brand)then
       BRANDS << brand
     end
   end
