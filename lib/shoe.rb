b986b68a7c63568d182f5ef24304f5b4d054109c
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-29 19:35:48 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,8 +6,8 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    if BRANDS.include(brand)
-      BRANDS << brand
+    if BRANDS.include(@brand)
+      BRANDS << @brand
     end
   end
 
